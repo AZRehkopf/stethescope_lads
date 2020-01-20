1b9f4d505ffb69531a1ef851eb2d3c937dcf3557
Andrew <8813337+AZRehkopf@users.noreply.github.com>
2020-01-20 03:38:30 -0500

    Python template

---
+++ python/steth_controller.py
@@ -0,0 +1,39 @@
+# steth_controller.py
+# Coordinates all the digital stethescope components
+
+### Imports ###
+
+# Built-ins
+import logging
+import os
+import sys
+
+# Local imports
+
+# Third party imports
+import bluetooth
+
+### Globals ###
+
+LOGGING_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs')
+
+if not os.path.isdir(LOGGING_DIR):
+    os.mkdir(LOGGING_DIR)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(asctime)s - %(message)s",
+    datefmt='%d/%m/%Y %H:%M:%S',
+    handlers=[
+        logging.FileHandler(os.path.join(LOGGING_DIR, 'python.log')),
+        logging.StreamHandler(sys.stdout)
+    ])
+
+LOGGER = logging.getLogger(__name__)
+
+class StethescopeController():
+    def __init__():
+        pass
+
+if __name__ == "__main__":
+    pass
